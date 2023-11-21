bc6a813ceeca3ec70aacf1c7c0c9837ece1c50e2
papengul <papengul@gmail.com>
2023-11-21 20:09:49 +0300

    Initial commit

---
+++ src/lesson10.java
@@ -0,0 +1,13 @@
+public class lesson10 {
+    public static void main(String[] arg) {
+        int x = 22;
+        int y = 33;
+        System.out.println(GetSumm(x,y));
+
+    }
+
+    public static int GetSumm(int num1, int num2) {
+        int RESULT = num1 + num2;
+        return RESULT;
+    }
+}
