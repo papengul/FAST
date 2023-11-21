c251bf0372ac455304ae64124d535ba9d7a84b14
papengul <papengul@gmail.com>
2023-11-21 21:12:00 +0300

    Initial commit

---
+++ src/lesson_11.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+public class lesson_11 {
+    public static void main(String[] arg) throws IOException {
+        int y = System.in.read();
+        char z = (char) y;
+
+        System.out.println(z);
+    }
+}
